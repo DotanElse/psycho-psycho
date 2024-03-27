337ed5f881447e594f6b1dc7f7a0b693756cbfff
DotanElse <dotanelse@gmail.com>
2024-03-27 15:33:42 +0200

    Changed sizes and wrong/correct indications

--- lib/pages/QuestionPage.dart
+++ lib/pages/QuestionPage.dart
@@ -62,17 +62,31 @@ class _QuestionPageState extends State<QuestionPage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  // Your answer options and submit button here
-                  buildAnswerOption(currentQuestion.optionA, 'A'),
-                  buildAnswerOption(currentQuestion.optionB, 'B'),
-                  buildAnswerOption(currentQuestion.optionC, 'C'),
-                  buildAnswerOption(currentQuestion.optionD, 'D'),
+                  // Row for options A and B
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      buildAnswerOption('1'),
+                      buildAnswerOption('2'),
+                    ],
+                  ),
+                  SizedBox(height: 16.0), // Adjust the spacing between rows
+                  // Row for options C and D
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      buildAnswerOption('3'),
+                      buildAnswerOption('4'),
+                    ],
+                  ),
                   SizedBox(height: 16.0),
-                  SizedBox(
-                    width: 150,
-                    child: ElevatedButton(
-                      onPressed: answerSubmitted ? loadRandomQuestion : submitAnswer,
-                      child: Text(answerSubmitted ? 'Next Question' : 'Submit'),
+                  Center(
+                    child: SizedBox(
+                      width: 150,
+                      child: ElevatedButton(
+                        onPressed: answerSubmitted ? loadRandomQuestion : submitAnswer,
+                        child: Text(answerSubmitted ? 'Next Question' : 'Submit'),
+                      ),
                     ),
                   ),
                 ],
@@ -90,31 +104,52 @@ class _QuestionPageState extends State<QuestionPage> {
     });
   }
 
-  Widget buildAnswerOption(String option, String optionLetter) {
-    Color? color;
-    if (answerSubmitted) {
-      if (optionLetter == currentQuestion.correct) {
-        color = Colors.green;
-      } else if (optionLetter == selectedAnswer) {
-        color = Colors.red;
+  Widget buildAnswerOption(String optionLetter) {
+    double buttonSize = MediaQuery.of(context).size.width * 0.4; // Adjust size as needed
+    Color selectedBorderColor = Colors.black; // Border color for selected button
+    double borderWidth = 4.0; // Border width
+    Color? disabledColor = Colors.grey;
+    if(answerSubmitted)
+      {
+        if(optionLetter == selectedAnswer)
+          {
+            disabledColor = Colors.red;
+          }
+        if(optionLetter == currentQuestion.correct)
+          {
+            disabledColor = Colors.green;
+          }
       }
-    }
 
-    return ListTile(
-      title: Text(
-        option,
-        style: TextStyle(color: color),
-      ),
-      leading: Radio(
-        value: optionLetter,
-        groupValue: selectedAnswer,
-        onChanged: answerSubmitted
-            ? null
-            : (value) {
+
+    return SizedBox(
+      width: buttonSize,
+      height: buttonSize,
+      child: ElevatedButton(
+        onPressed: answerSubmitted ? null : () {
           setState(() {
-            selectedAnswer = value.toString();
+            selectedAnswer = optionLetter;
           });
         },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Theme.of(context).colorScheme.primaryContainer, // Use your preferred color here
+          disabledBackgroundColor: disabledColor,
+          minimumSize: Size(buttonSize, buttonSize),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(buttonSize * 0.3), // Adjust borderRadius as needed
+            side: BorderSide(
+              color: selectedAnswer == optionLetter ? selectedBorderColor : Colors.transparent,
+              width: borderWidth,
+            ),
+          ),
+        ),
+        child: Text(
+          optionLetter,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimaryContainer,
+            fontSize: buttonSize * 0.5, // Adjust fontSize as needed
+          ),
+        ),
       ),
     );
   }
